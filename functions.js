37bd85ec10a4c5d4ce07088bfbab7a3c53bbfff6
reedo <info@reederror.com>
2018-10-10 22:31:39 +0100

    added section on callbacks

--- functions.js
+++ functions.js
@@ -25,3 +25,28 @@ curriedMultiply(3); //returns a function that takes an input (b) and will return
 curriedMultiply(3)(4); //returns 3 * 4
 
 /* ------------------------------------------------------------------------------------------------------------------------- */
+//compose
+
+const compose = (f, g) => a => f(g(a)); //feeds value/object a into function g, then feeds the output of g into function f
+
+/* ------------------------------------------------------------------------------------------------------------------------- */
+//callback funtions
+//non-blocking, so the code will continue on
+
+//there is an extra parameter called 'callback'. This must be a function. The output of processItem will be fed into
+//the function passed in by the call
+const processItem = (item, callback) => {
+  //do processing here
+  return callback(item); //item is passed into the callback function, then that function is returned
+};
+
+//so it would be called like this:
+const unprocessedItem = { name: 'item1' }; //just an object for demonstration purposes
+const processedItem = processItem(unprocessedItem, () => {
+  console.log(`${item.name} has been processed`); //this function is called when the processItem function has completed.
+  //                                                the main block of code will continue to execute, whilst the callback
+  //                                                function runs in the background
+  return item;
+});
+
+/* ------------------------------------------------------------------------------------------------------------------------- */
