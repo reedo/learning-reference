af275496ae0bac719a6326c3054bcc011741e4b0
reedo <info@reederror.com>
2018-10-10 21:23:11 +0100

    added section on currying

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 /* ------------------------------------------------------------------------------------------------------------------------- */
+//closures
 //keeps greet in its own scope, so its never mixed up with another variable called greet
 
 const first = () => {
@@ -6,10 +7,21 @@ const first = () => {
   const second = () => {
     alert(greet);
   };
-  return second;
+  return second; //the function that is returned will remember the value of greet at the point the function is returned,
+  //               so if the value of greet changes later, it will not affect the value of greet inside this function
 };
 
 const newFunc = first();
 newFunc();
+
+/* ------------------------------------------------------------------------------------------------------------------------- */
+//currying
+
+const multiply = (a, b) => a * b; //passes a and b into a function then returns a * b
+
+const curriedMultiply = a => b => a * b; //passes a into a function then passes a and b into a function
+//                                         then returns those values multiplied. Example:
+curriedMultiply(3); //returns a function that takes an input (b) and will return b * 3
+curriedMultiply(3)(4); //returns 3 * 4
 
 /* ------------------------------------------------------------------------------------------------------------------------- */
